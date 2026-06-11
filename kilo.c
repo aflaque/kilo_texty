cc368057ab74e0921cb290efeea2b22fb735d20e
aflaque <aflaque2003@gmail.com>
2026-06-11 17:42:35 +0530

    added seach highlighting

--- kilo.c
+++ kilo.c
@@ -39,7 +39,8 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL = 0,
-    HL_NUMBER
+    HL_NUMBER,
+    HL_MATCH
 };
 
 /*** data ***/
@@ -231,6 +232,7 @@ void editorUpdateSyntax(erow *row) {
 int editorSyntaxToColor(int hl) {
     switch (hl) {
         case HL_NUMBER: return 31;
+        case HL_MATCH: return 34;
         default: return 37;
     }
 }
@@ -492,6 +494,7 @@ void editorFindCallback(char *query, int key) {
             E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
+            memset(&row->hl[match - row->render], HL_MATCH,strlen(query));
             break;
         }
     }
