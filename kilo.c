23a8d3c4446700f307abb0d22a669ba44e15cc9b
aflaque <aflaque2003@gmail.com>
2026-06-12 19:51:09 +0530

    Added string highligting magenta color

--- kilo.c
+++ kilo.c
@@ -24,6 +24,7 @@
 #define KILO_TAB_STOP 4
 #define KILO_QUIT_TIMES 3
 #define HL_HIGHLIGHT_NUMBERS (1 << 0)
+#define HL_HIGHLIGHT_STRINGS (1 << 1)
 #define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
 
 enum editorKey {
@@ -41,6 +42,7 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL = 0,
+    HL_STRING,
     HL_NUMBER,
     HL_MATCH
 };
@@ -90,7 +92,7 @@ struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_extentsions,
-        HL_HIGHLIGHT_NUMBERS
+        HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
     },
 };
 
@@ -252,12 +254,34 @@ void editorUpdateSyntax(erow *row) {
 
     if (E.syntax == NULL) return;
     int prev_sep = 1;
+    int in_string = 0;
 
     int i = 0;
     while (i < row->rsize) {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
 
+        if (E.syntax->flag & HL_HIGHLIGHT_STRINGS) {
+            if (in_string) {
+                row->hl[i] = HL_STRING;
+                if (c == '\\' && i + 1 < row->rsize) {
+                    row->hl[i + 1] = HL_STRING;
+                    i += 2;
+                    continue;
+                }
+                if (c == in_string) in_string = 0;
+                i++;
+                prev_sep = 1;
+                continue;
+            } else {
+                if (c == '"' || c == '\'') {
+                    in_string = c;
+                    row->hl[i] = HL_STRING;
+                    i++;
+                    continue;
+                }
+            }
+        }
         if (E.syntax->flag & HL_HIGHLIGHT_NUMBERS) {
             if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
                 (c == '.' && prev_hl == HL_NUMBER)) {
@@ -275,6 +299,7 @@ void editorUpdateSyntax(erow *row) {
 int editorSyntaxToColor(int hl) {
     switch (hl) {
         case HL_NUMBER: return 31;
+        case HL_STRING: return 35;
         case HL_MATCH: return 34;
         default: return 37;
     }
